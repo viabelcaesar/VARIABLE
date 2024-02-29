bd4f0a0a4d1174a5e19f075f2cad05020b2c1ff1
viabelcaesar <160814850+viabelcaesar@users.noreply.github.com>
2024-02-29 11:16:43 +0700

    Add files via upload

---
+++ kasir_viabelcy.py
@@ -0,0 +1,19 @@
+#harga barang
+harga_roko=20000
+harga_minyak=25000
+harga_gula=11000
+
+#diskon
+diskon=10000
+
+#saldo
+saldo_misto=100000
+
+#total belanja
+total_belanja = harga_roko + harga_minyak + harga_gula - diskon
+ 
+
+ #kembalian
+kembalian=saldo_misto - total_belanja
+
+print("jumlah kembalian misto adalah:", kembalian)
---
+++ variabel.py
@@ -0,0 +1,30 @@
+#multi assignment
+
+a, b, c = 1, 2, "cek"
+
+print('a:', type(a))
+print('b:', type(b))
+print('c:', type(c))
+
+print("================")
+
+d, e, f= 10,10,10
+
+print('d:', d)
+print('e:', e)
+print('f:', f)
+
+
+panjang = 5
+lebar = 10,5
+luas = panjang * lebar
+
+print(panjang, '*', lebar, '-', luas)
+print("tipe dari variabel panjang:", type(panjang))
+print("tipe dari variabel panjang:", type(lebar))
+print("tipe dari variabel luas:", type(luas))
+
+
+
+
+
